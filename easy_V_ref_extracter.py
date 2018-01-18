21ea299be2392e3875c3eb043f6f4fc5d5f810eb
lrm13 <lrm13@uw.edu>
2018-01-18 15:25:26 -0800

    finally functional V_ref_extractor that takes in results files and prints out the energy differences and the uncertainity in the measurement

--- easy_V_ref_extracter.py
+++ easy_V_ref_extracter.py
@@ -4,59 +4,63 @@ import glob
 import sys
 
 ID=sys.argv[1]
-
+au2wn=219474.63 #cm-1*/(a.u.)
 #To Make V_ref:
 path='results*'+ID+'*'
 fileList=glob.glob(path)
 nRep=0
-for fileName in fileList:
-    resultsFile=open(fileName,'r')
-    for line in resultsFiles:
-        if 'propagating for time' in line:
-            nSteps=int(line.split()[3])
-            print 'nSteps=', nSteps
-            nWfns=int(line.split()[5])
-            print 'nWfns=',nWfns
 
-        if 'Repetition Number is' in line:
-            n=int(line.split()[3]) 
-            nRep=n if n>nRep else nRep
+resultsFile=open(fileList[0],'r')
+for line in resultsFile:
+    if 'propagating for time' in line:
+        nSteps=int(line.split()[3])
+        print 'nSteps=', nSteps
+        nWfns=int(line.split()[5])
+        print 'nWfns=',nWfns
+        
+    if 'Repetition Number is' in line:
+        n=int(line.split()[3]) 
+        nRep=n if n>nRep else nRep
 
+resultsFile.close()
 nRep=nRep+1 #nRep will be 9 if there are 10 reps complete
+nSteps=nSteps+1 #because n+1 are printed (25 prop steps=>26 numbers)
 
+print 'there are ', nRep,'repetitions'
+V_ref=np.zeros((nWfns, 3, nRep*len(fileList),nSteps)) 
+#Structure: number of wavefunctions, number of repetitions, number of propagation steps, number of states (ground,V1left, v1right)
+stateDict={}
+stateDict['0ground']=0
+stateDict['1left']=1
+stateDict['1right']=2
+for n,fileIn in enumerate(fileList):
+    resultsFile=open(fileIn,'r')
+    frep=int(fileIn.split('-')[3].split('.')[0])-1  #split by - then by .
+    print fileIn
+    for line in resultsFile:
+        if "state:" in line:
+            state=line.split()[1]+line.split()[3]
+        if "for wavefunction number" in line:
+            iwfn=int(line.split()[7])
+            rep=int(line.split()[3])
+            irep=nRep*frep+rep
+        if "DISCRETE:" in line:
+            istep=int(line.split()[4])
+            #print iwfn,stateDict[state],istep,';',nRep,'x',frep,'+',rep,'=',irep
+            V_ref[iwfn,stateDict[state],irep,istep]=float(line.split()[6])
 
+    resultsFile.close()
+V_ref=V_ref*au2wn
+averageV=np.zeros((nWfns,3))
+averageV_w_s=np.average(V_ref,axis=(2,3))
+print averageV.shape
+print 'MIN: ',np.min(averageV_w_s[:,0]),np.min(averageV_w_s[:,1]),np.min(averageV_w_s[:,2])
+print 'MAX: ',np.max(averageV_w_s[:,0]),np.max(averageV_w_s[:,1]),np.max(averageV_w_s[:,2])
+print 'UNC: ',uncertainityV_s
 
-path='V_ref*'+ID+'*'
-fileList=glob.glob(path)
+uncertainityV_s=[(np.max(averageV_w_s[:,0])-np.min(averageV_w_s[:,0]))/(2*np.sqrt(nWfns)),(np.max(averageV_w_s[:,1])-np.min(averageV_w_s[:,1]))/(2*np.sqrt(nWfns)),(np.max(averageV_w_s[:,2])-np.min(averageV_w_s[:,2]))/(2*np.sqrt(nWfns))]
+averageV_s=np.average(V_ref,axis=(0,2,3))
+print 'AVE: ', averageV_s
 
-#print fileList
-
-count=len(fileList)
-
-print count
-averageV1L=[]
-averageV1R=[]
-averageV0=[]
-eqStep=-5
-for fileName in fileList:
-    #print 'fileName',fileName
-    tempData=np.loadtxt(fileName)
-    stepNumber=tempData[:,0]
-    V1L=tempData[:,1]
-    V1R=tempData[:,2]
-    V0=tempData[:,3]
-    
-    averageV1L.append(np.average(V1L[eqStep:]))
-    averageV1R.append(np.average(V1R[eqStep:]))
-    averageV0.append(np.average(V0[eqStep:]))
-
-
-print 'V1L-V0=', (np.average(averageV1L)-np.average(averageV0))*219474.63
-print 'V1R-V0=', (np.average(averageV1L)-np.average(averageV0))*219474.63
-print 'V0=', (np.average(averageV0))*219474.63
-
-fileout=open('Energies.dat', 'w')
-fileout.write('V1L-V0= '+str((np.average(averageV1L)-np.average(averageV0))*219474.63)+'   cm^{-1}\n')
-fileout.write('V1R-V0= '+str((np.average(averageV1L)-np.average(averageV0))*219474.63)+'   cm^{-1}\n')
-fileout.write('V0=     '+str(np.average(averageV0)*219474.63)+'   cm^{-1}\n')
-fileout.close()
+deltaE=[averageV_s[1]-averageV_s[0],averageV_s[2]-averageV_s[0]]
+unc_delta_E=[np.sqrt(),np.sqrt()]
